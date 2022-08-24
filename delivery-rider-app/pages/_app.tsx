411c392049f34546dad9ebbc58db54cb26f3895c
unknown <zakumialex@gmail.com>
2022-08-25 00:38:42 +0630

    ADD:json-server and home page

--- delivery-rider-app/pages/_app.tsx
+++ delivery-rider-app/pages/_app.tsx
@@ -1,8 +1,32 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material"
 
+const theme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#ff0000",
+        },
+    },
+    typography: {
+        body1: {
+            fontWeight: 700,
+            fontSize: "1.2rem",
+        },
+        body2: {
+            fontWeight: 100,
+            color: "#0000005c",
+        },
+    },
+})
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
--- delivery-rider-app/pages/index.tsx
+++ delivery-rider-app/pages/index.tsx
@@ -1,17 +1,83 @@
-import type { NextPage } from "next"
-import Head from "next/head"
-import Image from "next/image"
-import {
-    Theme,
-    ThemeProvider,
-    createTheme,
-    Typography,
-    CssBaseline,
-    PaletteOptions,
-} from "@mui/material"
-import { red } from "@mui/material/colors"
-import { PaletteColorOptions } from "@mui/material"
-import { useState } from "react"
+import type { NextPage, GetStaticProps } from "next"
+import { Grid, Avatar, Typography, Badge } from "@mui/material"
+import api from "../axios-api/axios"
+import { AxiosResponse } from "axios"
+import homeStyle from "./index.module.scss"
+
+interface User {
+    username: string
+    email: string
+    imagePath: string
+}
+interface Props {
+    userCredentials: User
+}
+
+const Home: NextPage<Props> = ({ userCredentials }) => {
+    return (
+        <Grid container maxWidth={"600px"} height="100vh">
+            {/*  Container */}
+
+            {/* NavBar */}
+            <Grid
+                container
+                item
+                xs={12}
+                alignItems="center"
+                justifyContent="space-between"
+                className={homeStyle.navBar}
+            >
+                <Grid item>
+                    <Avatar
+                        alt="profile-pic"
+                        sx={{ width: 45, height: 45 }}
+                        src={userCredentials.imagePath}
+                    />
+                </Grid>
+                <Grid item></Grid>
+            </Grid>
+
+            {/* Map */}
+
+            <Grid container item xs={12} className={homeStyle.map}>
+                {/* DragUp Bar */}
+                <Grid container item xs={12} className={homeStyle.dragUpbar}>
+                    <Grid
+                        container
+                        item
+                        justifyContent="center"
+                        height="max-content"
+                    >
+                        <div className={homeStyle.pill} />
+                    </Grid>
+                    <Grid container item xs={12} justifyContent="space-between">
+                        <Typography variant="body2">Distance: 0.7km</Typography>
+
+                        <Typography variant="body2">(15 Min)</Typography>
+                    </Grid>
+                    <Grid container item xs={12}>
+                        <Typography variant="body1">
+                            No.10, 10th St, 10 Quarter, Hlaing
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Home
+
+export async function getStaticProps() {
+    const response: AxiosResponse = await api.get("/profile")
+    const data: User = response.data
+    return {
+        props: {
+            userCredentials: data,
+        },
+    }
+}
+
 /*
 Only if additional colors are necessary
      else use MUI 7*4 built-in theme colors to customize
@@ -33,27 +99,3 @@ customRedColor: {
                             // ↓↓  ↓↓  ↓↓  ↓↓  ↓↓  ↓↓
                             // Implementation ...Soon
                             */
-const theme = createTheme({
-    palette: {
-        mode: "dark",
-        primary: {
-            main: "#ff0000",
-        },
-        other: {
-            main: "#efefef",
-        },
-    },
-})
-const Home: NextPage = () => {
-    const [something, setSomthing] = useState("plalskfj")
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Typography color="primary">Login Page</Typography>
-        </ThemeProvider>
-    )
-}
-
-
-
-export default Home
